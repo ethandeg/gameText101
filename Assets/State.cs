b3761109fac64d619101bd0c8271f0ab62e7e004
Ethan Degenhardt <ethandeg1996@gmail.com>
2021-07-06 20:05:04 -0500

    added some placeholder rooms/state

--- Assets/State.cs
+++ Assets/State.cs
@@ -7,9 +7,14 @@ using UnityEngine;
 public class State : ScriptableObject
 {
     [TextArea(10,14)] [SerializeField] string storyText;
-
+    [SerializeField] State[] nextStates;
     public string GetStateStory()
     {
         return storyText;
+    }
+
+    public State[] getNextStates()
+    {
+        return nextStates;
     }
 }
